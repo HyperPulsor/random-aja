d8f92c0f911b77133c90e87a20250f9163409d2b
hyperpulsor <rakanfasya@gmail.com>
2023-10-15 09:32:51 +0700

    feat: implemented Demo 4

--- bacabaca/src/main/java/apap/tutorial/bacabaca/dto/request/ReadBukuResponseDTO.java
+++ bacabaca/src/main/java/apap/tutorial/bacabaca/dto/request/ReadBukuResponseDTO.java
@@ -1,8 +1,10 @@
 package apap.tutorial.bacabaca.dto.request;
+import apap.tutorial.bacabaca.model.Penulis;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class ReadBukuResponseDTO {
     private String tahunTerbit;
     private BigDecimal harga;
     private String namaPenerbit;
+    private List<Penulis> listPenulis;
 }
